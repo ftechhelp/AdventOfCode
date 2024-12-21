bd28d109b32b6b8344084e6995d4181f381b54d2
Vincent Fontaine <ftechhelp@gmail.com>
2024-12-21 14:05:19 -0800

    works on example but not input data

--- 2024/day_20/part1.py
+++ 2024/day_20/part1.py
@@ -32,41 +32,51 @@ class Racetrack:
                     visited.add((nx, ny))
                     queue.append(((nx, ny), steps + 1))
 
-        return -1  # return -1 if no path is found
+        return -1
 
-    def picoseconds_saved_with_cheat(self, start, goal):
-        queue = deque([(start, 0, False, set((start, False)))])
+    def picoseconds_saved_with_cheat_path_count(self, start, goal, picoseconds_greater_than = 0) -> int:
+
+        queue = deque([(start, 0, None, set([(start, None)]))]) # position, steps, cheat_pos, visited
 
         while queue:
-
             print(f"Queue Length: {len(queue)}")
 
-            (x, y), steps, cheat_used, visited = queue.popleft()
+            (x, y), steps, cheat_pos, visited = queue.popleft()
 
-            if steps >= self.shortest_path_picoseconds:
-                continue
-
+            # reached goal within specified picoseconds
             if (x, y) == goal:
                 self.shortest_paths_picoseconds_saved_with_cheat[self.shortest_path_picoseconds - steps] += 1
+                continue
+            
+            #stop if not faster then fastest non cheating time or 
+            if steps >= self.shortest_path_picoseconds or self.shortest_path_picoseconds - steps < picoseconds_greater_than:
                 continue
 
             for direction_x, direction_y in Navigate().directions:
                 next_position = Navigate().get_next_position((direction_x, direction_y), (x, y))
                 nx, ny = next_position
 
-                if Navigate().is_valid_position(grid, next_position) and (next_position, cheat_used) not in visited:
+                if Navigate().is_valid_position(self.grid, next_position):
 
                     if grid[ny][nx] != "#":
-                        visited.add((next_position, cheat_used))
-                        queue.append((next_position, steps + 1, cheat_used, visited))
+                        new_state = (next_position, cheat_pos)
 
-                    elif not cheat_used:
-                        visited.add((next_position, True))
-                        queue.append((next_position, steps + 1, True, visited))
+                        if new_state not in visited:
+                            new_visited = visited | {new_state}
+                            queue.append((next_position, steps + 1, cheat_pos, new_visited))
+
+                    elif cheat_pos is None:  
+                        new_state = (next_position, next_position)
+                        
+                        if new_state not in visited:
+                            new_visited = visited | {new_state}
+                            queue.append((next_position, steps + 1, next_position, new_visited))
+
+        return sum(self.shortest_paths_picoseconds_saved_with_cheat.values())
 
 raw_grid = []
 
-with open("example.txt") as f:
+with open("input_data.txt") as f:
     raw_grid = f.read().splitlines()
 
 grid = [list(row) for row in raw_grid]
@@ -76,5 +86,5 @@ racetrack = Racetrack(grid)
 shortest_path_steps = racetrack.shortest_path_picoseconds
 print(f"Shortest Path Steps: {shortest_path_steps}")
 
-racetrack.picoseconds_saved_with_cheat(racetrack.start, racetrack.end)
-print(f"Shortest Path Picoseconds Saved: {racetrack.shortest_paths_picoseconds_saved_with_cheat}")
+shortest_path_picosends_saved_with_cheat_path_count = racetrack.picoseconds_saved_with_cheat_path_count(racetrack.start, racetrack.end, 100)
+print(f"Shortest Path Picoseconds Saved with Cheat Path Count: {shortest_path_picosends_saved_with_cheat_path_count}")
